b6836b540745c7bf15970c04f7aa1c9193f33491
vetheve <victorien.etheve@outlook.fr>
2023-02-02 12:19:51 +0000

    "ADD: Implement and test Endpoints for Fetching Total Net Balance"

--- server/net_balance.js
+++ server/net_balance.js
@@ -54,13 +54,70 @@ netBalanceRouter
             }
         });
     });
-  
-netBalanceRouter
-  .route('/:year/:month')
-  // Get the total net balance for a specific year and month
 
 // Endpoint to handle requests for the balance by year
 netBalanceRouter
     .route('/:year')
     // Get the total net balance for a specific year
+    .get((req, res) => {
+        const year = parseInt(req.params.year);
+        // Connect to the PostgreSQL database using the connection pool
+        pool.query(`
+    -- Define the subquery to calculate the total amount of revenues
+    WITH revenue_total AS (
+    SELECT SUM(amount) as total_revenues
+    FROM revenues
+    WHERE extract(year from dt_value) = $1),
+    -- Define the subquery to calculate the total amount of expenses
+    expense_total AS (
+    SELECT SUM(amount) as total_expenses
+    FROM expenses
+    WHERE extract(year from dt_value) = $1)
+    -- Calculate the net balance by subtracting the total expenses from the total revenues
+    SELECT total_revenues - total_expenses as net_balance
+    FROM revenue_total, expense_total;`, [year], (err, result) => {
+            if (err) {
+                // If there was an error, return a 500 status code with an error message
+                res.status(500).json({
+                    error: "Error fetching budget balance"
+                });
+            } else {
+                // If the query was successful, return a 200 status code with the result
+                res.status(200).json(result.rows[0]);
+            }
+        });
+    });
 
+// Endpoint to handle requests for the balance by year and month    
+netBalanceRouter
+    .route('/:year/:month')
+    // Get the total net balance for a specific year and month
+    .get((req, res) => {
+        const year = parseInt(req.params.year);
+        const month = parseInt(req.params.month);
+        // Connect to the PostgreSQL database using the connection pool
+        pool.query(`
+    -- Define the subquery to calculate the total amount of revenues
+    WITH revenue_total AS (
+    SELECT SUM(amount) as total_revenues
+    FROM revenues
+    WHERE extract(year from dt_value) = $1 AND extract(month from dt_value) = $2),
+    -- Define the subquery to calculate the total amount of expenses
+    expense_total AS (
+    SELECT SUM(amount) as total_expenses
+    FROM expenses
+    WHERE extract(year from dt_value) = $1 AND extract(month from dt_value) = $2)
+    -- Calculate the net balance by subtracting the total expenses from the total revenues
+    SELECT total_revenues - total_expenses as net_balance
+    FROM revenue_total, expense_total;`, [year,month], (err, result) => {
+            if (err) {
+                // If there was an error, return a 500 status code with an error message
+                res.status(500).json({
+                    error: "Error fetching budget balance"
+                });
+            } else {
+                // If the query was successful, return a 200 status code with the result
+                res.status(200).json(result.rows[0]);
+            }
+        });
+    });
--- test/balance/net_balance_test.js
+++ test/balance/net_balance_test.js
@@ -34,3 +34,48 @@ test('4.1 Get /netbalance should returns the total net balance', async t => {
     // Asserting that the value of the 'Net balance' key is 5640
     t.is(res.body['net_balance'], '5640');
 });
+
+// Test to check if the Get request to '/netbalance/:year' route returns the total net balance by year
+test('4.2 Get /netbalance/:year should returns the total net balance by year', async t => {
+
+    // Selecting a year to test with
+    const year = '2021';
+
+    // Making a Get request to the '/netbalance/:year' route which does not exist yet
+    const res = await request(app).get(`/netbalance/${year}`);
+    
+    // Asserting that the status code of the response is 200
+    t.is(res.status, 200);
+    
+    // Print the object in the console
+    t.log(res.body)
+        
+    // Asserting that the response body is an object and contains the key 'Net balance'
+    t.true(typeof res.body === 'object' && 'net_balance' in res.body);
+    
+    // Asserting that the value of the 'Net balance' key is 5640
+    t.is(res.body['net_balance'], '5640');
+});
+
+// Test to check if the Get request to '/netbalance/:year/:month' route returns the total net balance by year and month
+test('4.3 Get /netbalance/:year/:month should returns the total net balance by year', async t => {
+
+    // Selecting a year and a month to test with
+    const year = '2021';
+    const month = '11';
+
+    // Making a Get request to the '/netbalance/:year/:month' route which does not exist yet
+    const res = await request(app).get(`/netbalance/${year}/${month}`);
+    
+    // Asserting that the status code of the response is 200
+    t.is(res.status, 200);
+    
+    // Print the object in the console
+    t.log(res.body)
+        
+    // Asserting that the response body is an object and contains the key 'Net balance'
+    t.true(typeof res.body === 'object' && 'net_balance' in res.body);
+    
+    // Asserting that the value of the 'Net balance' key is 470
+    t.is(res.body['net_balance'], '470');
+});
