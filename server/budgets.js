0102aa024f0960ef15930d9b1be134f6bc3e382f
vetheve <victorien.etheve@outlook.fr>
2023-02-03 10:13:13 +0000

    "ADD: Implement and test Endpoints - POST new budget and GET all budget."

--- server/budgets.js
+++ server/budgets.js
@@ -5,6 +5,16 @@ const budgetsRouter = require('express').Router();
 const bodyParser = require('body-parser');
 budgetsRouter.use(bodyParser.json());
 
+// Import the required module for creating a connection pool to a PostgreSQL database
+const Pool = require('pg').Pool
+
+// Create a connection pool instance with the given configuration options
+const pool = new Pool({
+    host: 'localhost',
+    database: 'balance_budget',
+    port: 5432,
+  });
+
 // Import functions from db.js
 const {
     getAllFromDatabase,
@@ -22,25 +32,62 @@ budgetsRouter
     .route('/')
     // Endpoint to get all budgets
     .get((req, res) => {
-        const allBudgets = getAllFromDatabase('budgets');
-        if (allBudgets) {
-            res.status(200).send(allBudgets);
-        } else {
-            res.status(404).send({
-                error: "No budgets not found"
-            });
-        }
+        // Connect to the PostgreSQL database using the connection pool
+        pool.query(`
+      -- Define the subquery to get the total records of budgets
+      SELECT * FROM budgets;`, (err, result) => {
+            if (err) {
+                // If there was an error, return a 500 status code with an error message
+                res.status(500).json({
+                    error: "Error fetching budgets"
+                });
+            } else {
+                // If the query was successful, return a 200 status code with the result
+                res.status(200).json(result.rows);
+            }
+        });
     })
-    // Add a new budget to the list
+    // Add a new budget to the table budgets
     .post((req, res) => {
-        const addedBudget = addBudgetToDatabase(req.body.id, req.body.category, req.body.amount);
-        if (addedBudget) {
-            res.status(201).send(addedBudget);
-        } else {
-            res.status(400).send({
-                error: "Failed to add budget"
-            });
-        }
+        // Destructure budget_id, category, and amount from the request body
+        const {
+            budget_id,
+            category,
+            amount
+        } = req.body;
+
+        // Generate a timestamp and convert it to an ISO string format
+        const timestamp = Date.now();
+        const isoString = new Date(timestamp).toISOString();
+
+        // Create a budget object with the provided information and default timestamp and ID values
+        const element = {
+            budget_id: budget_id,
+            dt_create: isoString,
+            dt_update: isoString,
+            dt_value: isoString,
+            category: category,
+            amount: amount
+        };
+
+        // Execute a query to insert the new budget into the budgets table
+        pool.query(`
+        INSERT INTO budgets (budget_id, dt_create, dt_update, dt_value, category, amount)
+        VALUES ($1, to_timestamp($2, 'YYYY-MM-DD"T"HH24:MI:SS.MS'), to_timestamp($3, 'YYYY-MM-DD"T"HH24:MI:SS.MS'), to_timestamp($4, 'YYYY-MM-DD"T"HH24:MI:SS.MS'), $5, $6);
+    `, [element.budget_id, element.dt_create, element.dt_update, element.dt_value, element.category, element.amount], (err, result) => {
+            // If there is an error, return a 500 status code with an error message
+            if (err) {
+                res.status(500).json({
+                    error: "Error adding budget"
+                });
+            }
+            // If the query is successful, return a 200 status code with a success message
+            else {
+                res.status(201).json({
+                    message: "Budget added successfully"
+                });
+            }
+        });
     });
 
 // Endpoint to handle requests to a specific budget resource by ID
---
+++ test/budgets/budgets_test.js
@@ -0,0 +1,76 @@
+const test = require("ava");  // Importing the AVA test library
+const request = require("supertest");  // Importing the supertest library for making HTTP requests
+const apiRouter = require('../../server/api.js');  // Importing the api router file for the all the routers
+const express = require('express'); // Importing express
+
+// Create an instance of the express application
+const app = express();
+
+// Use the apiRouter with the '/api' route
+app.use('/', apiRouter);
+
+// Import functions from db.js
+const {
+    getFromDatabaseByItem
+} = require('../../server/db.js');
+
+/*BUDGETS*/
+
+// Test to check if the GET request to '/budgets' route returns an array of all budgets
+test('1.1 GET /budgets should return an array of all budgets', async t => {
+
+    // Making a GET request to the '/budgets' route
+    const res = await request(app).get('/budgets');
+
+    // Asserting that the status code of the response is 200
+    t.is(res.status, 200);
+
+    // Asserting that the response body is an array
+    t.true(Array.isArray(res.body));
+
+    // Print the array in the console
+    //t.log(res.body)
+});
+
+// Test to check if the GET request to '/budgets/:id' route returns a specific budget by ID
+test('1.2 GET /budgets/:id should retrieve a specific budget with the given ID', async t => {
+
+    // Selecting a budget ID to test with
+    const budgetId = '2021-01 Monthly Housing Budget';
+    
+    // Making a GET request to the '/budgets/:id' route with the selected budget ID
+    const res = await request(app).get(`/budgets/${budgetId}`);
+    
+    // Asserting that the status code of the response is 200
+    t.is(res.status, 200);
+
+    // Print the object in the console
+    t.log(res.body)
+    
+    // Asserting that the response body is an object and contains the selected budget ID
+    t.true(typeof res.body === 'object');
+
+    // Asserting that the response body contains the selected budget ID
+    t.true(res.body.id === budgetId);
+});
+
+// Test to check if the POST request to '/budgets' route add a new budget in the list budgets
+test('1.3 POST /budgets should add a new budget to the list', async t => {
+
+    // Creating a new budget object to add
+    const newBudget = {
+        budget_id: "2022-01 Monthly Food Budget",
+        category: "food",
+        amount: "0"
+    };
+
+    // Making a POST request to the '/budgets' route with the new budget object
+    const res = await request(app).post('/budgets').send(newBudget);
+
+    // Asserting that the status code of the response is 201
+    t.is(res.status, 201);
+
+    // Print the object in the console
+    t.log(res.body)
+
+});
